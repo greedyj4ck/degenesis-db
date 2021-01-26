995ad46909aa07ab4e16db798592bc147aabfbd2
Sławomir Szczęsny <s_szczesny@outlook.com>
2021-01-26 21:45:42 +0100

    fixes for first import

--- scripts/import.js
+++ scripts/import.js
@@ -362,3 +362,135 @@ Items.update;
 return weapons;
 
 }
+
+// "brawl" : "brawl",
+ // "armedMelee" : "melee",
+
+
+ game.importDataArmor= function() {
+  let weapons = []
+  fetch("systems/degenesis/import/armor.json").then(r => r.json().then(json => {
+  for (let item of json.items)
+  {
+    let itemData = duplicate(game.system.model.Item.armor)
+    itemData.cult = item.cult.join(", ")
+    itemData.AP = item.rating
+    itemData.description = item.description
+    itemData.encumbrance = item.encumbrance || 0
+    itemData.tech = item.technology || 0
+    itemData.group = ""
+
+    itemData.qualities = item.qualities.map(q => {
+      let qualityObj = {
+        name : q.name = DEG_Utility.findKey(q.name, DEGENESIS.armorQualities),
+      }
+      qualityObj.values = DEGENESIS.armorQualitiesValues[qualityObj.name].map(name => {return {name}})
+      if (q.property)
+        qualityObj.values[0].value = q.property
+      return qualityObj
+    })
+    itemData.resources = item.resources || 0
+    itemData.slots = {total: item.slots || 0, used : 0}
+    itemData.value = item.value || 0
+    let newItem = {name : item.name, data : itemData, type : "armor"}
+    if (item.specialty)
+      setProperty(newItem, "flags.degenesis.specialty", item.specialty)
+
+  Item.create(newItem);
+  Items.update;
+      
+     weapons.push(newItem);
+  }
+}))
+
+return weapons;
+
+}
+
+game.importDataShield= function() {
+let weapons = []
+fetch("systems/degenesis/import/shields.json").then(r => r.json().then(json => {
+for (let item of json.items)
+{
+  let itemData = duplicate(game.system.model.Item.shield)
+  itemData.cult = item.cult.join(", ")
+  itemData.defense.D = item.defense[0]["active"]
+  itemData.defense.p_defense = item.defense[1]["passive"]
+  itemData.attack.D = item.attack
+
+  itemData.description = item.description
+  itemData.encumbrance = item.encumbrance || 0
+  itemData.tech = item.technology || 0
+  itemData.group = ""
+
+  itemData.qualities = item.qualities.map(q => {
+    let qualityObj = {
+      name : q.name = DEG_Utility.findKey(q.name, DEGENESIS.armorQualities),
+    }
+    qualityObj.values = DEGENESIS.armorQualitiesValues[qualityObj.name].map(name => {return {name}})
+    if (q.property)
+      qualityObj.values[0].value = q.property
+    return qualityObj
+  })
+  itemData.resources = item.resources || 0
+  itemData.slots = {total: item.slots || 0, used : 0}
+  itemData.value = item.value || 0
+  let newItem = {name : item.name, data : itemData, type : "shield"}
+  if (item.specialty)
+    setProperty(newItem, "flags.degenesis.specialty", item.specialty)
+
+Item.create(newItem);
+Items.update;
+    
+   weapons.push(newItem);
+}
+}))
+
+return weapons;
+
+}
+
+game.importDataAmmo= function() {
+let weapons = []
+fetch("systems/degenesis/import/ammunition.json").then(r => r.json().then(json => {
+for (let item of json.items)
+{
+  let itemData = duplicate(game.system.model.Item.ammunition)
+  
+  
+  itemData.description = item.description
+  itemData.damage = item.damage || 0 
+  itemData.tech = item.technology || 0
+  itemData.value = item.price || 0
+ 
+
+
+ /*  itemData.qualities = item.qualities.map(q => {
+    let qualityObj = {
+      name : q.name = DEG_Utility.findKey(q.name, DEGENESIS.armorQualities),
+    }
+    qualityObj.values = DEGENESIS.armorQualitiesValues[qualityObj.name].map(name => {return {name}})
+    if (q.property)
+      qualityObj.values[0].value = q.property
+    return qualityObj
+  }) */
+
+  let newItem = {name : item.ammo, data : itemData, type : "ammunition"}
+  
+  /* if (item.specialty)
+    setProperty(newItem, "flags.degenesis.specialty", item.specialty) */
+
+Item.create(newItem);
+Items.update;
+    
+   weapons.push(newItem);
+}
+}))
+
+return weapons;
+
+}
+
+
+
+
